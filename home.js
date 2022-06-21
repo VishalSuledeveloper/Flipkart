7ca93fb3c7cc61c81ca41def78c7fddf1356f02a
Vishal <vssule@mitaoe.ac.in>
2022-06-21 12:51:56 +0530

    code

---
+++ home.js
@@ -0,0 +1,25 @@
+
+// window.addEventListener("load",function(){
+//     setTimeout(
+//         function open(event){
+//         document.querySelector.apply(".coupon").style.
+//         display=block;
+//     },
+//     2000
+//     )
+// })
+
+// document.querySelector("#close").addEventListener
+// ("click", function(){
+//     document.querySelector(".coupon").style.display = "none";
+// })
+
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible'
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden'
+}
+
+
